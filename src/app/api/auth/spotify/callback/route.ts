7b02dee5733826f0332e66705ba7f2a877a7ae1f
Thomas Heniart <heniart.thomas@gmail.com>
2025-02-05 18:40:03 -0400

    fix: redirection uri encoding

--- src/app/api/auth/spotify/callback/route.ts
+++ src/app/api/auth/spotify/callback/route.ts
@@ -22,7 +22,7 @@ export async function GET(request: Request) {
       new URLSearchParams({
         grant_type: "authorization_code",
         code,
-        redirect_uri: encodeURIComponent(process.env.SPOTIFY_REDIRECT_URI!),
+        redirect_uri: process.env.SPOTIFY_REDIRECT_URI!,
         client_id: process.env.SPOTIFY_CLIENT_ID!,
         client_secret: process.env.SPOTIFY_CLIENT_SECRET!,
       }),
